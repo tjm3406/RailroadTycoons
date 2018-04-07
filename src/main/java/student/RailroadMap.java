3f22c59152c7a494ab2640a156d9b08a20d4cbf5
Pord123 <pedroantbretonca@hotmail.com>
2018-04-07 15:32:08 -0400

    Implement RailroadMap class
    Took 35 seconds

--- src/main/java/student/RailroadMap.java
+++ src/main/java/student/RailroadMap.java
@@ -1,7 +1,14 @@
 package student;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
 import model.Baron;
+import model.Orientation;
 import model.RailroadMapObserver;
 
 /**
@@ -9,108 +16,110 @@ import model.RailroadMapObserver;
  */
 public class RailroadMap implements model.RailroadMap {
 
+  private List<RailroadMapObserver> observers;
+  private model.Space[][] grid;
+  private TreeSet<model.Route> routes;
+  private TreeSet<model.Route> unclaimedRoutes;
+  private int rows;
+  private int cols;
 
-  /**
-   * Adds the specified {@linkplain RailroadMapObserver observer} to the map. The observer will be
-   * notified of significant events involving this map such as when a {@linkplain Route route} has
-   * been claimed by a {@linkplain Baron}.
-   *
-   * @param observer The {@link RailroadMapObserver} being added to the map.
-   */
-  @Override
-  public void addObserver(RailroadMapObserver observer) {
+
+  public RailroadMap(ArrayList<Route> routes) {
+    this.observers = new ArrayList<>();
+    this.routes = new TreeSet<>(Comparator.comparingInt(model.Route::getLength));
+    this.unclaimedRoutes = new TreeSet<>(Comparator.comparingInt(model.Route::getLength));
+
+    this.rows = routes.stream().max(Comparator.comparingInt(a -> a.getDestination().getRow())).get()
+        .getDestination().getRow() + 1;
+    this.cols = routes.stream().max(Comparator.comparingInt(a -> a.getDestination().getCol())).get()
+        .getDestination().getCol() + 1;
+
+    grid = new model.Space[rows][cols];
+
+    for (Route route : routes) {
+      this.routes.add(route);
+      this.unclaimedRoutes.add(route);
+      grid[route.getOrigin().getRow()][route.getOrigin().getCol()] = route.getOrigin();
+      grid[route.getDestination().getRow()][route.getDestination().getCol()] = route
+          .getDestination();
+      for (model.Track track : route.getTracks()) {
+        grid[track.getRow()][track.getCol()] = track;
+      }
+    }
+
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == null) {
+          grid[i][j] = new Space(i, j);
+        }
+      }
+
+    }
+
 
   }
 
-  /**
-   * Removes the specified {@linkplain RailroadMapObserver observer} from the map. The observer will
-   * no longer be notified of significant events involving this map.
-   *
-   * @param observer The observer to remove from the collection of registered observers that will be
-   * notified of significant events involving this map.
-   */
+  public void addObserver(RailroadMapObserver observer) {
+    observers.add(observer);
+
+  }
+
   @Override
   public void removeObserver(RailroadMapObserver observer) {
+    observers.remove(observer);
 
   }
 
-  /**
-   * Returns the number of rows in the map. This is determined by the location of the southernmost
-   * {@linkplain Station station} on the map.
-   *
-   * @return The number of rows in the map.
-   */
+
   @Override
   public int getRows() {
-    return 0;
+    return rows;
   }
 
-  /**
-   * Returns the number of columns in the map. This is determined by the location of the easternmost
-   * {@linkplain Station station} on the map.
-   *
-   * @return The number of columns in the map.
-   */
+
   @Override
   public int getCols() {
-    return 0;
+    return cols;
   }
 
-  /**
-   * Returns the {@linkplain Space space} located at the specified coordinates.
-   *
-   * @param row The row of the desired {@link Space}.
-   * @param col The column of the desired {@link Space}.
-   * @return The {@link Space} at the specified location, or null if the location doesn't exist on
-   * this map.
-   */
   @Override
   public model.Space getSpace(int row, int col) {
-    return null;
+    return grid[row][col];
   }
 
-  /**
-   * Returns the {@linkplain Route route} that contains the {@link Track track} at the specified
-   * location (if such a route exists}.
-   *
-   * @param row The row of the location of one of the {@link Track tracks} in the route.
-   * @param col The column of the location of one of the {@link Track tracks} in the route.
-   * @return The {@link Route} that contains the {@link Track} at the specified location, or null if
-   * there is no such {@link Route}.
-   */
   @Override
   public model.Route getRoute(int row, int col) {
+    if (grid[row][col] instanceof model.Track) {
+      Track track = (Track) grid[row][col];
+      return track.getRoute();
+    }
     return null;
   }
 
-  /**
-   * Called to update the {@linkplain RailroadMap map} when a {@linkplain Baron} has claimed a
-   * {@linkplain Route route}.
-   *
-   * @param route The {@link Route} that has been claimed.
-   */
   @Override
   public void routeClaimed(model.Route route) {
+    unclaimedRoutes.remove(route);
+    for (RailroadMapObserver observer : observers) {
+      observer.routeClaimed(this, route);
+    }
 
   }
 
-  /**
-   * Returns the length of the shortest unclaimed {@linkplain Route route} in the map.
-   *
-   * @return The length of the shortest unclaimed {@link Route}.
-   */
   @Override
   public int getLengthOfShortestUnclaimedRoute() {
-    return 0;
+    return unclaimedRoutes.first().getLength();
   }
 
-  /**
-   * Returns all of the {@link Route Routes} in this map.
-   *
-   * @return A {@link Collection} of all of the {@link Route Routes} in this RailroadMap.
-   */
   @Override
   public Collection<model.Route> getRoutes() {
-    return null;
+    return routes;
+  }
+
+  public List<RailroadMapObserver> getObservers() {
+    return observers;
+  }
+
+  public TreeSet<model.Route> getUnclaimedRoutes() {
+    return unclaimedRoutes;
   }
 }
