f1c37b9f2fa5b0763b13d3ef6c69051fe9116957
Pord123 <pedroantbretonca@hotmail.com>
2018-04-07 21:11:24 -0400

    Create Pair class
    Took 27 seconds

---
+++ src/main/java/student/Pair.java
@@ -0,0 +1,47 @@
+package student;
+
+import model.Card;
+
+/**
+ * Concrete implementation of Pair Interface
+ *
+ * @author PedroBreton
+ */
+public class Pair implements model.Pair {
+
+  private Card firstCard;
+  private Card secondCard;
+
+  /**
+   * Construct an instance of the Pair class
+   *
+   * @param firstCard first Card
+   * @param secondCard second Card
+   */
+  public Pair(Card firstCard, Card secondCard) {
+    this.firstCard = firstCard;
+    this.secondCard = secondCard;
+  }
+
+  /**
+   * Returns the first {@linkplain Card card} in the pair. Note that, if the game deck is empty, the
+   * value of this card may be {@link Card#NONE}.
+   *
+   * @return The first {@link Card} in the pair.
+   */
+  @Override
+  public Card getFirstCard() {
+    return firstCard;
+  }
+
+  /**
+   * Returns the second {@linkplain Card card} in the pair. if the game deck is empty, the value of
+   * this card may be {@link Card#NONE}.
+   *
+   * @return The second {@link Card} in the pair.
+   */
+  @Override
+  public Card getSecondCard() {
+    return secondCard;
+  }
+}
