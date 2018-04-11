9377f87c3bd6f0cb75b418dabae3ffb07db77585
tjm3406 <Harbor98>
2018-04-11 19:35:06 -0400

    Finish Player.getScore

--- src/main/java/student/Player.java
+++ src/main/java/student/Player.java
@@ -1,5 +1,6 @@
 package student;
 
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -250,8 +251,33 @@ public class Player implements model.Player {
   public int getScore() {
     score = 0;
     for(model.Route route : routes) {
-      
+      int length = route.getLength();
+
+      switch (length) {
+        case 1:
+          score++;
+          break;
+        case 2:
+          score += 2;
+          break;
+        case 3:
+          score += 4;
+          break;
+        case 4:
+          score += 7;
+          break;
+        case 5:
+          score += 10;
+          break;
+        case 6:
+          score += 15;
+          break;
+        default:
+          score += 5 * (length-3);
+
+      }
     }
+    return score;
   }
 
   /**
