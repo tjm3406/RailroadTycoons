05f16507f4b0b86f77068ac62f2c5d2db54d633f
tjm3406 <Harbor98>
2018-04-11 20:24:16 -0400

    Fix getScore

--- src/main/java/student/Player.java
+++ src/main/java/student/Player.java
@@ -259,33 +259,11 @@ public class Player implements model.Player {
   @Override
   public int getScore() {
     score = 0;
+
     for(model.Route route : routes) {
-      int length = route.getLength();
-
-      switch (length) {
-        case 1:
-          score++;
-          break;
-        case 2:
-          score += 2;
-          break;
-        case 3:
-          score += 4;
-          break;
-        case 4:
-          score += 7;
-          break;
-        case 5:
-          score += 10;
-          break;
-        case 6:
-          score += 15;
-          break;
-        default:
-          score += 5 * (length-3);
-
-      }
+      score += route.getPointValue();
     }
+
     return score;
   }
 
